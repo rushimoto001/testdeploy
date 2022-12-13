3369dfab8e1b408750fc08e8e0c0ee5eef03b6e5
Rushi Moto <rushimoto001@gmail.com>
2022-12-13 09:02:47 +0530

    [skip ci]

--- update.sh
+++ update.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-while true; do echo 'update'; sleep 2s; done &
 sleep 55m && git clone https://${SECRET_TOKEN}@github.com/rushimoto001/testdeploy -b circleci-project-setup gitpush && cd gitpush && git config --global user.email "rushimoto001@gmail.com" && git config --global user.name "rushimoto001" && git config --global color.ui true && perl -i -pe '/^ver=/ && s/(\d+)/$&+1/e' count.txt && git add . && git commit --allow-empty -m "Update Software Version Count on $(date -u +"%D %T%p")" && git push -q
